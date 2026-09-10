f2e97582f81ae6710cdbb0e55ea168c70517945b
hamilton2959 <skytech2959@gmail.com>
2026-09-10 15:02:35 +0300

    init commit

---
+++ random/index.js
@@ -0,0 +1,14 @@
+
+function generatePassword(Length, includeLowercase, includeUppercase, includeNumbers, includeSymbols) {
+    return '';
+}
+
+const passwordLength = 12;
+const includeLowercase = true;
+const includeUppercase = true;
+const includeNumbers = true;
+const includeSymbols = true;
+
+const password = generatePassword(passwordLength, includeLowercase, includeUppercase, includeNumbers, includeSymbols);
+
+console.log(`Generated password: ${password}`);
